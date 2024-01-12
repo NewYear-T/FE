3ec4ee30d2a169ebc91fb3d17fe10521fc452973
kasterra <kasterra@naver.com>
2024-01-12 21:38:50 +0900

    보호/비보호 레이아웃 구현

---
+++ app/routes/_withoutLogin.login.tsx
@@ -0,0 +1,13 @@
+import Header from "~/components/Header";
+
+const Login = () => {
+  return (
+    <Header
+      title="로그인"
+      subtitle="다짐메이트를 사용하기위해 로그인해요!"
+      logo=""
+    />
+  );
+};
+
+export default Login;
